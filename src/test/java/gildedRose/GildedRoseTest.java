88bb74af13a415cb580eafe47b615bbf6eb8bcdb
ZHANGGA5 <enju648454858>
2019-08-13 15:56:50 +0800

    test: pass the test of should_nothing_change_when_call_updateQuality_given_SULFURAS

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -4,19 +4,25 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class GildedRoseTest {
-    private final String AGED_BRIE= "Aged Brie";
-    private final String BACKSTAGE_PASSES= "Backstage passes to a TAFKAL80ETC concert";
-    private final String SULFURAS= "Sulfuras, Hand of Ragnaros";
+    private final String MISMATCH = "MISMATCH";
+    private final String AGED_BRIE = "Aged Brie";
+    private final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+    private final String SULFURAS = "Sulfuras, Hand of Ragnaros";
 
     GildedRose gildedRose = new GildedRose(null);
     Item[] items = new Item[1];
 
     @Test
-    public void should_decrease_sellIn_when_call_updateQuality_given_AGRD_item_has_one_sell_in_and_beyond_fifty_quality(){
+    public void should_nothing_change_when_call_updateQuality_given_SULFURAS(){
+        givenAnItemToGildedRose(new Item(SULFURAS,1,1));
+        assertAnItemSellInAndQualityWhenCallUpdateQuality(1,1);
+    }
+
+    @Test
+    public void should_decrease_sellIn_when_call_updateQuality_given_AGRD_item_has_one_sellIn_and_beyond_fifty_quality(){
         givenAnItemToGildedRose(new Item(AGED_BRIE,1,51));
         assertAnItemSellInAndQualityWhenCallUpdateQuality(0,51);
     }
-
 
 
     private void givenAnItemToGildedRose(Item item){
