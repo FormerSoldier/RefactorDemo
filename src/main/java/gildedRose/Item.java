2d0fc27cef08a6098e4d21735d5c11ee339ffc86
ZHANGGA5 <enju648454858>
2019-08-13 17:21:21 +0800

    Refact: package the for loop each row to a method and put the method to Item class

--- src/main/java/gildedRose/Item.java
+++ src/main/java/gildedRose/Item.java
@@ -18,4 +18,51 @@ public class Item {
    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
+
+    public void UpdateEachItem(GildedRose gildedRose) {
+        if (!name.equals("Aged Brie")
+                && !name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (quality > 0 && !name.equals("Sulfuras, Hand of Ragnaros")) {
+                quality = quality - 1;
+            }
+        } else {
+            if (quality < 50) {
+                quality = quality + 1;
+
+                if (name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (sellIn < 11) {
+                        increaseQualityIfQualityLessThanFifty();
+                    }
+
+                    if (sellIn < 6) {
+                        increaseQualityIfQualityLessThanFifty();
+                    }
+                }
+            }
+        }
+
+        if (!name.equals("Sulfuras, Hand of Ragnaros")) {
+            sellIn = sellIn - 1;
+        }
+
+        if (sellIn < 0) {
+            if (!name.equals("Aged Brie")) {
+                if (!name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (quality > 0 && !name.equals("Sulfuras, Hand of Ragnaros")) {
+                        quality = quality - 1;
+                    }
+                } else {
+                    quality = 0;
+                }
+            } else {
+                increaseQualityIfQualityLessThanFifty();
+            }
+        }
+    }
+
+    public void increaseQualityIfQualityLessThanFifty() {
+        if (quality < 50) {
+            quality = quality + 1;
+        }
+    }
 }
